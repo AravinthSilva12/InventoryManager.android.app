226a10747bc76fa458dc83c584aaf1cecfd866bd
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2026-03-02 21:06:42 +0530

    Feb25 - Project struture established. March2 - updated - pending is BottomNav class!

--- app/src/main/java/com/aravinth/inventorymanager/ui/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/aravinth/inventorymanager/ui/navigation/BottomNavigationBar.kt
@@ -1,33 +1,12 @@
 package com.aravinth.inventorymanager.ui.navigation
-
-import android.graphics.drawable.Icon
-import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.compose.runtime.Composable
-
-data class BottomNavItem(
-    val screen: Screen,
-    val icon: ImageVector,
-    val label: String
-)
-
-@Composable
-fun BottomNavigationBar(navController: NavHostController){
-    val items = listOf(BottomNavItem(Screen.Home, Icons.Default.Home, "Home"),
-    BottomNavItem(Screen.Stock, Icons.Default.List, "Stock"),
-    BottomNavItem(Screen.Billing, Icons.Default.ShoppingCart, "Billing"),
-    BottomNavItem(Screen.Suppliers, Icons.Default.person, "Suppliers"),
-    BottomNavItem(Screen.CRM, Icons.Default.Star, "CRM")
-    )
-}
-val currentRoute = navController.currentBackStateEntryAsState().value?.destination?.route
-
-     NavigationBar {
-         items.forEach { item ->
-         }
-     }
+import androidx.navigation.NavHostController
