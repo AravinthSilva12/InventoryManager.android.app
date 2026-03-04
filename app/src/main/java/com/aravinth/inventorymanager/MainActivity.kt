5e38fd9aba982229e21870e74008337af1ac7ae2
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2026-03-04 18:54:49 +0530

    Feb25 - Project struture established. March2 - updated - pending is BottomNav class!          updated - MainScreen, MainActivity classes made as test kits!!          MainScreen & MainActivity classes are to be made on march3. March3 - updated - MainAc, MainS, SplashS.          Tested and updated.          Icons, logo updated.   Latest update! *Splash screen is removed and kept as reserve. Nav bar padding is solved. UI navigation is finally smooth!

--- app/src/main/java/com/aravinth/inventorymanager/MainActivity.kt
+++ app/src/main/java/com/aravinth/inventorymanager/MainActivity.kt
@@ -2,13 +2,12 @@ package com.aravinth.inventorymanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.aravinth.inventorymanager.ui.navigation.MainScreen
-import com.aravinth.inventorymanager.ui.theme.InventoryManagerTheme
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.aravinth.inventorymanager.ui.navigation.MainScreen
 import com.aravinth.inventorymanager.ui.navigation.Screen
-import com.aravinth.inventorymanager.ui.screen.SplashScreen
+import com.aravinth.inventorymanager.ui.theme.InventoryManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
@@ -18,8 +17,7 @@ class MainActivity : ComponentActivity() {
             InventoryManagerTheme() {
                 val navController = rememberNavController()
                 NavHost(navController = navController,
-                    startDestination = Screen.Splash.route) {
-                    composable(Screen.Splash.route) { SplashScreen(navController) }
+                    startDestination = Screen.Main.route) {
                     composable(Screen.Main.route) {
                         MainScreen()
                     }
--- app/src/main/java/com/aravinth/inventorymanager/ui/navigation/MainScreen.kt
+++ app/src/main/java/com/aravinth/inventorymanager/ui/navigation/MainScreen.kt
@@ -1,11 +1,10 @@
 package com.aravinth.inventorymanager.ui.navigation
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.material3.ScaffoldDefaults.contentWindowInsets
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
