ae82aef4324898f659082403ed2ef824d61375a5
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2026-03-05 15:56:37 +0530

    Feb25 - Project struture established. March2 - updated - pending is BottomNav class!          updated - MainScreen, MainActivity classes made as test kits!!          MainScreen & MainActivity classes are to be made on march3. March3 - updated - MainAc, MainS, SplashS.          Tested and updated.          Icons, logo updated.   Latest update! *Splash screen is removed and kept as reserve. Nav bar padding is solved. UI navigation is finally smooth!   March 5th - updated with Stock item class & Stock repo interface, InMemory StockRepo class is finished.

--- app/src/main/java/com/aravinth/inventorymanager/data/repository/InMemoryStockRepository.kt
+++ app/src/main/java/com/aravinth/inventorymanager/data/repository/InMemoryStockRepository.kt
@@ -1,4 +1,31 @@
 package com.aravinth.inventorymanager.data.repository
+import com.aravinth.inventorymanager.domain.model.StockItem
+import com.aravinth.inventorymanager.domain.repository.StockRepository
 
-class InMemoryStockRepository {
+class InMemoryStockRepository: StockRepository {
+    private val items = mutableListOf<StockItem>()
+    // Add stock item :
+    override fun addStockItem(item: StockItem){
+        items.add(item)
+    }
+
+    // Get all stock items :
+    override fun getAllStockItems(): List<StockItem> {
+        return items
+    }
+
+    // Get stock item by id :
+    override fun getStockItemById(id: Int): StockItem? {
+        return items.find { it.id == id }
+    }
+    // Delete stock item by id :
+    override fun deleteStockItem(id:Int){
+        items.removeIf { it.id == id }
+    }
+    // Update stock item :
+    override fun updateStockItem(item : StockItem) {
+        val index = items.indexOfFirst { it.id == item.id }
+        if(index != -1)
+            items[index] = item
+    }
 }
