d11231ab126499b294b344356231ec2251e2a4e0
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2026-03-06 14:20:35 +0530

    Feb25 - Project struture established. March2 - updated - pending is BottomNav class!          updated - MainScreen, MainActivity classes made as test kits!!          MainScreen & MainActivity classes are to be made on march3. March3 - updated - MainAc, MainS, SplashS.          Tested and updated.          Icons, logo updated.   Latest update! *Splash screen is removed and kept as reserve. Nav bar padding is solved. UI navigation is finally smooth!   March 5th - updated with Stock item class & Stock repo interface, InMemory StockRepo class is finished!!   March 6th : Updated! StockUsecase & StockScreen classes are updated.

--- app/src/main/java/com/aravinth/inventorymanager/domain/usecase/StockUseCase.kt
+++ app/src/main/java/com/aravinth/inventorymanager/domain/usecase/StockUseCase.kt
@@ -1,14 +1,38 @@
 package com.aravinth.inventorymanager.domain.usecase
 import com.aravinth.inventorymanager.domain.model.StockItem
 import com.aravinth.inventorymanager.domain.repository.StockRepository
-import com.aravinth.inventorymanager.data.repository.InMemoryStockRepository
 
 class StockUseCase (private val repository: StockRepository) {
-    fun addStock(){
-
+    // add item :
+    fun addStock(item: StockItem) {
+        repository.addStockItem(item)
     }
-
-
-
-
+    // get all items :
+    fun getAllStockItems():List<StockItem> {
+        return repository.getAllStockItems()
+    }
+    // get item by id :
+    fun getStockById(id: Int): StockItem? {
+        return repository.getStockItemById(id)
+    }
+    // delete item :
+    fun deleteStock(id: Int){
+        repository.deleteStockItem(id)
+    }
+    // update item :
+    fun updateStock(item: StockItem) {
+        repository.updateStockItem(item)
+    }
+    // get low stock items :
+    fun getLowStockItems(): List<StockItem> {
+     return repository.getAllStockItems().filter { it.quantity <= it.reorderLevel }
+    }
+    // get In stock items :
+    fun getInStockItems(): List<StockItem> {
+        return repository.getAllStockItems().filter { it.quantity > 0 }
+    }
+    // get Out of stock items :
+    fun getOutOfStockItems(): List<StockItem> {
+    return repository.getAllStockItems().filter { it.quantity == 0 }
+    }
 }
--- app/src/main/java/com/aravinth/inventorymanager/ui/screen/StockScreen.kt
+++ app/src/main/java/com/aravinth/inventorymanager/ui/screen/StockScreen.kt
@@ -1,15 +1,51 @@
 package com.aravinth.inventorymanager.ui.screen
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.Alignment
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
-import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.aravinth.inventorymanager.data.repository.InMemoryStockRepository
+import com.aravinth.inventorymanager.domain.usecase.StockUseCase
 
 @Composable
 fun StockScreen(){
-    Box( modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text("Stock screen")
-       }
+    val repository = InMemoryStockRepository()
+    val useCase = StockUseCase(repository)
+
+    val allItems = useCase.getAllStockItems()
+    val lowStockItems = useCase.getLowStockItems()
+
+    LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        item {
+            Text(
+                text = "Stock Dashboard",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        //Low Stock section :
+        item {
+            Text(text = "Low stock items", fontWeight = FontWeight.Bold)
+        }
+            items(lowStockItems) { item ->
+                Text("${item.name}   (Qty: ${item.quantity})",
+                modifier = Modifier.padding(vertical = 4.dp))
+            }
+
+        // All stockItems section :
+        item { Spacer(modifier = Modifier.height(16.dp))
+               Text(text = "All stock items", fontWeight = FontWeight.Bold) }
+            items(allItems) { item ->
+                Text("${item.name}  (Qty: ${item.quantity})",
+                modifier = Modifier.padding(vertical = 4.dp))
+            }
+    }
 }
