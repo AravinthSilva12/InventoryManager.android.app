c0242544973e1c328c3be46116efa73408df2b7b
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2026-03-10 10:08:10 +0530

    Feb25 - Project struture established. March2 - updated - pending is BottomNav class!          updated - MainScreen, MainActivity classes made as test kits!!          MainScreen & MainActivity classes are to be made on march3. March3 - updated - MainAc, MainS, SplashS.          Tested and updated.          Icons, logo updated.   Latest update! *Splash screen is removed and kept as reserve. Nav bar padding is solved. UI navigation is finally smooth!   March 5th - updated with Stock item class & Stock repo interface, InMemory StockRepo class is finished!!   March 6th : Updated! StockUsecase & StockScreen classes are updated.   March 9th work half completed**   March 10th - work going**

--- app/src/main/java/com/aravinth/inventorymanager/ui/screen/AddStockScreen.kt
+++ app/src/main/java/com/aravinth/inventorymanager/ui/screen/AddStockScreen.kt
@@ -1,7 +1,13 @@
 package com.aravinth.inventorymanager.ui.screen
-
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.aravinth.inventorymanager.viewmodel.StockViewModel
 
-class AddStockScreen(navController: NavController) {
-Text(text)
+@Composable
+fun AddStockScreen(navController: NavController) {
+      val viewModel: StockViewModel = viewModel()
+
+
+
 }
--- app/src/main/java/com/aravinth/inventorymanager/ui/screen/StockScreen.kt
+++ app/src/main/java/com/aravinth/inventorymanager/ui/screen/StockScreen.kt
@@ -5,6 +5,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,41 +18,46 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
+import com.aravinth.inventorymanager.ui.navigation.Screen
 import com.aravinth.inventorymanager.viewmodel.StockViewModel
 
 @Composable
-fun StockScreen(navController: NavHostController) {
+fun StockScreen(navController: NavController) {
     val viewModel: StockViewModel = viewModel()
     val allItems = viewModel.getAllStockItems()
     val lowStockItems = viewModel.getLowStockItems()
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = {navController.navigate(Screen.AddStock.route)})
+        { Icon(Icons.Default.Add, contentDescription = "Add Stock") }
+    })
+      {padding ->
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(padding).padding(16.dp)) {
+            item {
+                Text(text = "Stock Dashboard", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.height(16.dp))
+            }
 
-    LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        item {
-            Text(text = "Stock Dashboard", fontSize = 22.sp, fontWeight = FontWeight.Bold)
-            Spacer(modifier = Modifier.height(16.dp))
-        }
+            // Low stock items :
+            item {
+                Text(text = "Low Stock Items", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+            }
+            items(lowStockItems) { item ->
+                Text(
+                    "${item.name} (Qty: ${item.quantity})",
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+            }
 
-        // Low stock items :
-        item {
-            Text(text = "Low Stock Items", fontSize = 18.sp, fontWeight = FontWeight.Bold)
-        }
-        items(lowStockItems) { item ->
-            Text(
-                "${item.name} (Qty: ${item.quantity})",
-                modifier = Modifier.padding(vertical = 4.dp)
-            )
-        }
-
-        // All stock items :
-        item {
-            Text(text = "All Stock Items", fontSize = 18.sp, fontWeight = FontWeight.Bold)
-        }
-        items(allItems) { item ->
-            Text(
-                "${item.name}  (Qty: ${item.quantity})",
-                modifier = Modifier.padding(vertical = 4.dp)
-            )
+            // All stock items :
+            item {
+                Text(text = "All Stock Items", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+            }
+            items(allItems) { item ->
+                Text(
+                    "${item.name}  (Qty: ${item.quantity})",
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+            }
         }
     }
 }
