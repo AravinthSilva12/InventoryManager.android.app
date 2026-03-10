2e474cfdf77a8e7879e5248429ef79a97560e1ca
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2026-03-10 12:04:11 +0530

    Feb25 - Project struture established. March2 - updated - pending is BottomNav class!          updated - MainScreen, MainActivity classes made as test kits!!          MainScreen & MainActivity classes are to be made on march3. March3 - updated - MainAc, MainS, SplashS.          Tested and updated.          Icons, logo updated.   Latest update! *Splash screen is removed and kept as reserve. Nav bar padding is solved. UI navigation is finally smooth!   March 5th - updated with Stock item class & Stock repo interface, InMemory StockRepo class is finished!!   March 6th : Updated! StockUsecase & StockScreen classes are updated.   March 9th work half completed**   March 10th - work going**

--- app/src/main/java/com/aravinth/inventorymanager/ui/screen/AddStockScreen.kt
+++ app/src/main/java/com/aravinth/inventorymanager/ui/screen/AddStockScreen.kt
@@ -1,13 +1,69 @@
 package com.aravinth.inventorymanager.ui.screen
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.aravinth.inventorymanager.domain.model.StockItem
 import com.aravinth.inventorymanager.viewmodel.StockViewModel
+import androidx.compose.foundation.text.input.InputTransformation.Companion.keyboardOptions
+import android.R.attr.value
+import androidx.compose.foundation.layout.height
 
 @Composable
 fun AddStockScreen(navController: NavController) {
-      val viewModel: StockViewModel = viewModel()
+    val viewModel: StockViewModel = viewModel()
+    // State variables :
+    var name by remember { mutableStateOf("") }
+    var purchasePrice by remember { mutableStateOf("") }
+    var sellingPrice by remember { mutableStateOf("") }
+    var quantity by remember { mutableStateOf("") }
+    var reorderLevel by remember { mutableStateOf("") }
 
-
-
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)){
+        Text(text = "Add Stock Item", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = name, onValueChange = {name = it}, label = {Text("Item Name")})
+        Spacer(modifier = Modifier.height(12.dp))
+        OutlinedTextField(value = purchasePrice, onValueChange = {purchasePrice = it}, label = {Text("Purchasing price")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+        Spacer(modifier = Modifier.height(12.dp))
+        OutlinedTextField(value = sellingPrice, onValueChange = {sellingPrice = it}, label = {Text("Selling Price")},
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+        Spacer(modifier = Modifier.height(12.dp))
+        OutlinedTextField(value = quantity, onValueChange = {quantity = it}, label = {Text("Quantity of the item")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+        Spacer(modifier = Modifier.height(12.dp))
+        OutlinedTextField(value = reorderLevel, onValueChange = {reorderLevel = it}, label = {Text("Re-Order level")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = {val item = StockItem(id = 0,
+                                               name = name,
+                                               purchasePrice = purchasePrice.toDouble(),
+                                               sellingPrice = sellingPrice.toDouble(),
+                                               quantity = quantity.toInt(),
+                                               reorderLevel = reorderLevel.toInt(),
+                                               supplierId = null
+                                               )
+                                viewModel.addStockItem(item)
+                                navController.popBackStack()
+                          }){
+            Text("Save")
+        }
+    }
 }
