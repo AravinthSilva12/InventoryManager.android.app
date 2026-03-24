0ee92be80e69a1a83054292f3e7837111b2f5bc0
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2026-03-24 17:51:43 +0530

    Feb25 - Project struture established. March2 - updated - pending is BottomNav class!          updated - MainScreen, MainActivity classes made as test kits!!          MainScreen & MainActivity classes are to be made on march3. March3 - updated - MainAc, MainS, SplashS.          Tested and updated.          Icons, logo updated.   Latest update! *Splash screen is removed and kept as reserve. Nav bar padding is solved. UI navigation is finally smooth!   March 5th - updated with Stock item class & Stock repo interface, InMemory StockRepo class is finished!!   March 6th : Updated! StockUsecase & StockScreen classes are updated.   March 9th work half completed**   March 10th - Save button working; edit, delete, filter are pending**   March 11th - Add, delete, edit, update (full CRUD) is completed!**   March 12th Stock filtering is completed; Bugs on StockScreen is fully fixed. **Database setup is pending.   March 18th - stock screen UI polished, still need works.   ** MVP UI for Stock screen is completed.   ** Database setup is next!   **StockViewModel.kt needs to be started tomorrow!!   March 19th : DB finally working and bugs are fixed!**   *Form UI polished, delete dialog fixed*   March 21 : Bottom nav bar refined**   **Mar21 :!Stock Module (mvp) is fully complete!**   March 23 : Billing module started**   March 24 : work going on at Billing**              BillScreen is pending*!!

--- app/src/main/java/com/aravinth/inventorymanager/ui/screen/BillScreen.kt
+++ app/src/main/java/com/aravinth/inventorymanager/ui/screen/BillScreen.kt
@@ -1,4 +1,31 @@
 package com.aravinth.inventorymanager.ui.screen
 
-class BillScreen {
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.aravinth.inventorymanager.viewmodel.BillViewModel
+
+@Composable
+fun BillScreen(navController: NavController){
+    val billViewModel: BillViewModel = viewModel()
+    val billItems by remember { mutableStateListOf<BillViewModel>( billViewModel.items ) }
+    val total = billViewModel.loadItems()
+    LaunchedEffect(Unit) { billViewModel.loadItems() }
+    Scaffold(modifier = Modifier.fillMaxSize())
+    { innerPadding->
+        LazyColumn() { }
+
+
+
+
+
+
+    }
 }
--- app/src/main/java/com/aravinth/inventorymanager/viewmodel/BillViewModel.kt
+++ app/src/main/java/com/aravinth/inventorymanager/viewmodel/BillViewModel.kt
@@ -2,12 +2,54 @@ package com.aravinth.inventorymanager.viewmodel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.AndroidViewModel
 import android.app.Application
+import androidx.compose.runtime.mutableStateListOf
 import androidx.navigation.NavController
+import com.aravinth.inventorymanager.data.repository.InMemoryBillRepository
+import com.aravinth.inventorymanager.domain.model.Bill
+import com.aravinth.inventorymanager.domain.model.BillItem
+import com.aravinth.inventorymanager.domain.repository.BillRepository
 import com.aravinth.inventorymanager.domain.usecase.BillUseCase
+import kotlinx.coroutines.launch
 
 class BillViewModel(application: Application) :
-    AndroidViewModel(application)
-    {
-        private val useCase: BillUseCase
-        private val billViewModel: BillViewModel
+    AndroidViewModel(application) {
+    //Dependencies:
+    private val repository = InMemoryBillRepository()
+    private val useCase = BillUseCase(repository)
+
+    //UI state:
+    private val _items = mutableStateListOf<BillItem>()
+    val items: List<BillItem> get() = _items
+    private var _total = 0.0
+    val total: Double get() = _total
+
+    //Initialize:
+    init {
+        loadItems()
     }
+
+    //Load:
+    fun loadItems(){
+        val result = useCase.getBillItems()
+        _items.clear()
+        _items.addAll(result)
+        _total = useCase.getTotal()
+    }
+
+    //CRUD:
+    fun addItem(item: BillItem) {
+            useCase.addItem(item)
+        loadItems()
+    }
+
+    fun clearBill() {
+            useCase.clearBill()
+         loadItems()
+    }
+
+    fun generateBill(): Bill {
+            val bill = useCase.generateBill()
+            clearBill()
+            return bill
+    }
+}
