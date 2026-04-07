b93d719b853412bcde96c3d5cf5b21955a6c9850
Aravinthasamy Rajkumar <aravinthasamy281099@gmail.com>
2026-04-07 19:13:32 +0530

    Remove duplicate stock DAO ID query method

--- app/src/main/java/com/aravinth/inventorymanager/data/local/StockDao.kt
+++ app/src/main/java/com/aravinth/inventorymanager/data/local/StockDao.kt
@@ -20,8 +20,6 @@ interface StockDao {
     @Query("SELECT * FROM stock_items WHERE id = :id")
     suspend fun getById(id: Int): StockItem?
 
-    @Query("SELECT*FROM stock_items WHERE id = :id")
-    suspend fun getStockItemById(id:Int): StockItem?
 
     @Query("UPDATE stock_items SET quantity = :newQty WHERE id = :id")
     suspend fun updateStockQuantity(id:Int, newQty:Int)
